c6d49119828e53e71dea2ab5563108d98d4ad502
Tharaka Nandakumara <jenteksolutions@gmail.com>
2018-12-07 10:53:48 -0600

    Added a method to clean the system. Not complete. Taken out the flowe sensor code and added it to a method

--- Arduino/Full/Full.ino
+++ Arduino/Full/Full.ino
@@ -1,6 +1,6 @@
 // Adding Libraries
 // Adding a comment to check the Git
-//New Comment 01 added
+
 #include <Adafruit_GFX.h>    // Core graphics library
 #include <MCUFRIEND_kbv.h>   // Hardware-specific library
 MCUFRIEND_kbv tft;
@@ -144,42 +144,7 @@ void loop(void)
 
       while (totalMilliLitres < numval - 50) {
 
-        if ((millis() - oldTime) > 1000)   // Only process counters once per second
-        {
-          // Disable the interrupt while calculating flow rate and sending the value to
-          // the host
-          detachInterrupt(sensorInterrupt);
-
-          // Because this loop may not complete in exactly 1 second intervals we calculate
-          // the number of milliseconds that have passed since the last execution and use
-          // that to scale the output. We also apply the calibrationFactor to scale the output
-          // based on the number of pulses per second per units of measure (litres/minute in
-          // this case) coming from the sensor.
-          flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
-
-          // Note the time this processing pass was executed. Note that because we've
-          // disabled interrupts the millis() function won't actually be incrementing right
-          // at this point, but it will still return the value it was set to just before
-          // interrupts went away.
-          oldTime = millis();
-
-          // Divide the flow rate in litres/minute by 60 to determine how many litres have
-          // passed through the sensor in this 1 second interval, then multiply by 1000 to
-          // convert to millilitres.
-          flowMilliLitres = (flowRate / 60) * 1000;
-
-          // Add the millilitres passed in this second to the cumulative total
-          totalMilliLitres += flowMilliLitres;
-
-          unsigned int frac;
-
-          // Reset the pulse counter so we can start incrementing again
-          pulseCount = 0;
-
-          // Enable the interrupt again now that we've finished sending output
-          attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
-          Serial.println(totalMilliLitres);
-        }
+      sensorOutput();
       }
       Serial.println("Stop Val **************************************************************");
       Serial.print(totalMilliLitres);
@@ -222,6 +187,44 @@ void showmsgXY(int x, int y, int sz, const GFXfont * f, const char *msg)
   tft.print(msg);
 
 }
+void sensorOutput(){
+          if ((millis() - oldTime) > 1000)   // Only process counters once per second
+        {
+          // Disable the interrupt while calculating flow rate and sending the value to
+          // the host
+          detachInterrupt(sensorInterrupt);
+
+          // Because this loop may not complete in exactly 1 second intervals we calculate
+          // the number of milliseconds that have passed since the last execution and use
+          // that to scale the output. We also apply the calibrationFactor to scale the output
+          // based on the number of pulses per second per units of measure (litres/minute in
+          // this case) coming from the sensor.
+          flowRate = ((1000.0 / (millis() - oldTime)) * pulseCount) / calibrationFactor;
+
+          // Note the time this processing pass was executed. Note that because we've
+          // disabled interrupts the millis() function won't actually be incrementing right
+          // at this point, but it will still return the value it was set to just before
+          // interrupts went away.
+          oldTime = millis();
+
+          // Divide the flow rate in litres/minute by 60 to determine how many litres have
+          // passed through the sensor in this 1 second interval, then multiply by 1000 to
+          // convert to millilitres.
+          flowMilliLitres = (flowRate / 60) * 1000;
+
+          // Add the millilitres passed in this second to the cumulative total
+          totalMilliLitres += flowMilliLitres;
+
+          unsigned int frac;
+
+          // Reset the pulse counter so we can start incrementing again
+          pulseCount = 0;
+
+          // Enable the interrupt again now that we've finished sending output
+          attachInterrupt(sensorInterrupt, pulseCounter, FALLING);
+          Serial.println(totalMilliLitres);
+        }
+  }
 void screenOne() {
   showmsgXY(50, 40, 1, &FreeSans9pt7b, "Auto Filling System");
 
@@ -236,7 +239,12 @@ void def() {
 }
 // Clean the machine
 void cleanMachine() {
-  digitalWrite(solonoid, LOW);
+ 
+  while (totalMilliLitres < 1000) {
+      sensorOutput();
+      digitalWrite(solonoid, LOW);
+      
+      }
   
 }
 String enterNum(char customKey) {
